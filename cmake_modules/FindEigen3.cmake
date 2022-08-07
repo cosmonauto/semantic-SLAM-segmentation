dfcc899c7d480705ab24d7f4d77d93b30e2bc50d
cosmonauto <cosmonautoyu@proton.me>
2022-08-07 15:06:32 +0000

    Setup CI

--- cmake_modules/FindEigen3.cmake
+++ cmake_modules/FindEigen3.cmake
@@ -49,4 +49,12 @@ macro(_eigen3_check_version)
   if(NOT EIGEN3_VERSION_OK)
 
     message(STATUS "Eigen3 version ${EIGEN3_VERSION} found in ${EIGEN3_INCLUDE_DIR}, "
-        
+                   "but at least version ${Eigen3_FIND_VERSION} is required")
+  endif(NOT EIGEN3_VERSION_OK)
+endmacro(_eigen3_check_version)
+
+if (EIGEN3_INCLUDE_DIR)
+
+  # in cache already
+  _eigen3_check_version()
+  set(EIGEN3_FOUND ${EIGEN3_VE
