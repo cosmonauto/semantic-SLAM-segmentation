7e05ba56a56d5b212d0cc929666f140ab1332ed7
cosmonauto <cosmonautoyu@proton.me>
2022-08-24 15:11:40 +0000

    added some missing files

--- experiment/exp_mapping.cpp
+++ experiment/exp_mapping.cpp
@@ -1,2 +1,11 @@
 #include "rgbdframe.h"
-#include "track
+#include "track.h"
+#include "pose_graph.h"
+#include "common_headers.h"
+#include "mapper.h"
+#include "readGTPose.h"
+#include "vo_stereo.hpp"
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/common_head
