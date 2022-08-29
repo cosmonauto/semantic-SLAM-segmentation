af8ccd2a2cac25f91830705600971a8ec090c993
cosmonauto <cosmonautoyu@proton.me>
2022-08-29 14:29:30 +0000

    fff

--- experiment/exp_mapping.cpp
+++ experiment/exp_mapping.cpp
@@ -23,4 +23,8 @@ int main()
     double f = parameterReader.getData<double>("camera.fx");
     double c_u = parameterReader.getData<double>("camera.cx");
     double c_v = parameterReader.getData<double>("camera.cy");
-    double base = parameterR
+    double base = parameterReader.getData<double>("camera.baseline");
+    double inlier_threshold = parameterReader.getData<double>("inlier_threshold");
+    voparam.calib.f  = f;      voparam.calib.cu = c_u;
+    voparam.calib.cv = c_v;    voparam.base     = base;	
+ 
