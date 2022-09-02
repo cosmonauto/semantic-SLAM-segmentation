97699726e785da81caddb68297c79166caa482d6
cosmonauto <cosmonautoyu@proton.me>
2022-09-02 11:50:35 +0000

    changed labels

--- experiment/reading_frame.cpp
+++ experiment/reading_frame.cpp
@@ -1,1 +1,14 @@
-#inc
+#include "rgbdframe.h"
+
+using namespace rgbd_tutor;
+
+int main()
+{    
+    ParameterReader para;
+    FrameReader     fr(para);
+
+    int i = 1 ;
+    while( RGBDFrame::Ptr frame = fr.next() )
+    {
+        char _filename[256];
+   
