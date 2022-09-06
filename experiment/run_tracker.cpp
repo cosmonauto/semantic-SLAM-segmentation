902c0783d417683f5b8d1e7f5b77774788c042bd
cosmonauto <cosmonautoyu@proton.me>
2022-09-06 19:47:10 +0000

    Updated translations.

--- experiment/run_tracker.cpp
+++ experiment/run_tracker.cpp
@@ -2,4 +2,16 @@
 #include "rgbdframe.h"
 #include "track.h"
 #include "readGTPose.h"
-#include "vo
+#include "vo_stereo.hpp"
+
+using namespace std;
+using namespace rgbd_tutor;
+
+int main()
+{
+    cout<<"running tracker..."<<endl;
+    ParameterReader para;
+    FrameReader frameReader( para );
+
+    VisualOdometryStereo::parameters voparam; 
+ 
