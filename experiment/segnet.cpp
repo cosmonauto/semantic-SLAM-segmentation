e38104627084bbcef1d9a4282a0d35b1364983f3
cosmonauto <cosmonautoyu@proton.me>
2022-09-27 06:25:45 +0000

    Increment

--- experiment/segnet.cpp
+++ experiment/segnet.cpp
@@ -8,4 +8,14 @@
 #include <dirent.h>
 #include <thread>
 #include <time.h>
-#i
+#include "common.h"
+#include "segnet.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // Load network
+    Classifier classifier;
+    string colorfile = "../models/color.png";
+    cv::Mat color =
