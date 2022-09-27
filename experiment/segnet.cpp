ae9f669a4b34b4b5ab544a4a34eba92ebf89758f
cosmonauto <cosmonautoyu@proton.me>
2022-09-27 15:27:04 +0000

    6th

--- experiment/segnet.cpp
+++ experiment/segnet.cpp
@@ -18,4 +18,12 @@ int main(int argc, char** argv)
     // Load network
     Classifier classifier;
     string colorfile = "../models/color.png";
-    cv::Mat color =
+    cv::Mat color = cv::imread(colorfile, 1);
+
+    // Load image
+    for (int i = 1550; i < 2309; i++)
+    {
+        char file_name[256];
+
+#if (datasetvalue==0)
+        //sprintf(file_name, "../dataset/05/image_3/%06d.png",
