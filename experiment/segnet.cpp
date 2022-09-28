1155eb754b5b963ebde0e852d1e1846d99527f4b
cosmonauto <cosmonautoyu@proton.me>
2022-09-28 11:39:04 +0000

    f2fs: set errno when f2fs_iget failed in recover_dentry

--- experiment/segnet.cpp
+++ experiment/segnet.cpp
@@ -26,4 +26,9 @@ int main(int argc, char** argv)
         char file_name[256];
 
 #if (datasetvalue==0)
-        //sprintf(file_name, "../dataset/05/image_3/%06d.png",
+        //sprintf(file_name, "../dataset/05/image_3/%06d.png",i);
+        sprintf(file_name, "/home/relaybot/Mu_Link/KittiData/05/image_2/%06d.png",i);
+#elif (datasetvalue==1)
+        sprintf(file_name, "../Image_Test/mu/rgb/%04d.jpg",i);
+#else
+        cout<<"没有定义数据集"<<end
