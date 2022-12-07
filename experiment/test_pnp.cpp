cd034a5b8eba73a2978edc272ddd743ae68c3f0a
cosmonauto <cosmonautoyu@proton.me>
2022-12-07 10:51:50 +0000

    issues #64 technical debt

--- experiment/test_pnp.cpp
+++ experiment/test_pnp.cpp
@@ -10,4 +10,12 @@ int main()
     cout<<"running test pnp"<<endl;
     ParameterReader para;
     FrameReader frameReader( para );
-   
+    OrbFeature  orb(para);
+    PnPSolver   pnp(para, orb);
+
+    RGBDFrame::Ptr refFrame = frameReader.next();
+    orb.detectFeatures( refFrame );
+    Eigen::Isometry3d   speed = Eigen::Isometry3d::Identity();
+
+    // plot
+    string pose_file 
