3e17df2a799334c872b5109e133c8f92918f9d8c
cosmonauto <cosmonautoyu@proton.me>
2022-12-07 09:22:13 +0000

    Prepare 0.8.4 release

--- experiment/test_pnp.cpp
+++ experiment/test_pnp.cpp
@@ -1,2 +1,13 @@
 #include "pnp.h"
-#includ
+#include "common_headers.h"
+#include "readGTPose.h"
+
+using namespace std;
+using namespace rgbd_tutor;
+
+int main()
+{
+    cout<<"running test pnp"<<endl;
+    ParameterReader para;
+    FrameReader frameReader( para );
+   
