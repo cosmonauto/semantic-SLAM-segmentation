c2d3bd798e3386d8891cc2a303d2b6b60f66403b
cosmonauto <cosmonautoyu@proton.me>
2023-01-01 06:28:50 +0000

    n/a

--- include/common_headers.h
+++ include/common_headers.h
@@ -1,2 +1,19 @@
 #ifndef COMMON_HEADERS_H
-#define COMMON_HEAD
+#define COMMON_HEADERS_H
+
+/**
+  * 各种可能用到的头文件，放在一起方便include
+  */
+
+// C++标准库
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <dirent.h>
+#include <time.h>
+#include <string>
+using namespace std;
+
+// Eigen
+#include <Ei
