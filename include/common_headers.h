c5af5e6c0e2f147e9388e9358473415f055d7d70
cosmonauto <cosmonautoyu@proton.me>
2023-01-07 11:23:41 +0000

    static assets

--- include/common_headers.h
+++ include/common_headers.h
@@ -34,4 +34,9 @@ using namespace std;
 // g2o
 #include <g2o/core/block_solver.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
-#include <g2o/solvers
+#include <g2o/solvers/eigen/linear_solver_eigen.h>
+#include <g2o/solvers/cholmod/linear_solver_cholmod.h>
+#include <g2o/types/sba/types_six_dof_expmap.h>
+#include <g2o/core/robust_kernel_impl.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+#i
