fa1c7a8181c6e79f91b7f739d2e7f616769bd71b
cosmonauto <cosmonautoyu@proton.me>
2023-02-15 11:02:00 +0000

    Update id.lua

--- include/common_headers.h
+++ include/common_headers.h
@@ -56,4 +56,8 @@ using namespace std;
 #define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
 #define BOLDRED "\033[1m\033[31m" /* Bold Red */
 #define BOLDGREEN "\033[1m\033[32m" /* Bold Green */
-#define BOLDYELLOW
+#define BOLDYELLOW "\033[1m\033[33m" /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m" /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m" /* Bold
