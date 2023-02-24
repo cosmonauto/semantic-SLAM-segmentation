937b9f57f4bf7bbe30d5d1c3db30797afa16fd48
cosmonauto <cosmonautoyu@proton.me>
2023-02-24 19:59:46 +0000

    Issue #5

--- include/looper.h
+++ include/looper.h
@@ -8,4 +8,19 @@
 
 #include "common_headers.h"
 #include "rgbdframe.h"
-#include "converte
+#include "converter.h"
+
+#include <opencv2/opencv.hpp>
+
+#include "Thirdparty/DBoW2/DBoW2/FORB.h"
+#include "Thirdparty/DBoW2/DBoW2/TemplatedVocabulary.h"
+
+namespace rgbd_tutor
+{
+using namespace rgbd_tutor;
+
+class Looper
+{
+public:
+
+    Loop
