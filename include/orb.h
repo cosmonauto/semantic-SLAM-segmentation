cb08d5122cecb81c9ad84afaf2b7dcafaed5baff
cosmonauto <cosmonautoyu@proton.me>
2023-04-01 16:39:01 +0000

    closes #4

--- include/orb.h
+++ include/orb.h
@@ -3,4 +3,19 @@
 
 #include "common_headers.h"
 #include "rgbdframe.h"
-#include "Thirdparty/orbslam_modified/include/ORBextra
+#include "Thirdparty/orbslam_modified/include/ORBextractor.h"
+
+/**
+ * OrbFeature
+ * 对orbslam2的特征部分进行一次封装，具有提取、匹配特征的功能
+ */
+
+namespace rgbd_tutor
+{
+
+class OrbFeature
+{
+public:
+    OrbFeature( const rgbd_tutor::ParameterReader& para )
+    {
+        int features  = para
