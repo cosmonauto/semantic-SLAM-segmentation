6de4dbb7759090819d313001746cd972a7bd75cd
cosmonauto <cosmonautoyu@proton.me>
2023-04-04 12:06:32 +0000

    Minor corrections

--- include/pose_graph.h
+++ include/pose_graph.h
@@ -1,4 +1,18 @@
 #ifndef POSE_GRAPH_H
 #define POSE_GRAPH_H
 #include "common_headers.h"
-#include "paramete
+#include "parameter_reader.h"
+#include "rgbdframe.h"
+#include "looper.h"
+#include "pnp.h"
+#include "orb.h"
+#include "track.h"
+
+#include <thread>
+#include <mutex>
+#include <functional>
+#include <map>
+#include <condition_variable>
+
+/**
+  * The pose g
