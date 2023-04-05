a2c086b78732339ee183cac99474171b26519e75
cosmonauto <cosmonautoyu@proton.me>
2023-04-05 12:19:59 +0000

    Dependency update.

--- include/pose_graph.h
+++ include/pose_graph.h
@@ -15,4 +15,7 @@
 #include <condition_variable>
 
 /**
-  * The pose g
+  * The pose graph performs global optimization.
+  * 在tracker跟踪当前帧currentFrame，返回一个粗略的位姿时，会把这个帧尝试放入pose graph中.
+  * pose graph将它与自己的refframe比较，得到一个相对位移估计。当这个估计大于给定阈值时，将该帧作为新的frame插入到pose graph中。
+  * pose graph本身有一个优化线程，负责查找相
