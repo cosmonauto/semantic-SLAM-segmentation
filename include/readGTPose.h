b6dd9e9cebdd866d463e89b3faf69f86afa2eccd
cosmonauto <cosmonautoyu@proton.me>
2023-05-23 12:49:39 +0000

    added spec files

--- include/readGTPose.h
+++ include/readGTPose.h
@@ -1,2 +1,11 @@
 #include <iostream>
-#include <fs
+#include <fstream>
+using namespace std;
+
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+//the following are UBUNTU/LINUX ONLY terminal color
+#define RESET "\033[0m"
+#define BLACK "\033[30m" /* 
