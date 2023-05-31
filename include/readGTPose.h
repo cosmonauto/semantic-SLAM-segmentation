114b4f671988c7c79428ba3960082b1ea1804e4a
cosmonauto <cosmonautoyu@proton.me>
2023-05-31 10:21:46 +0000

    xml fix

--- include/readGTPose.h
+++ include/readGTPose.h
@@ -14,4 +14,9 @@ using namespace std;
 #define YELLOW "\033[33m" /* Yellow */
 #define BLUE "\033[34m" /* Blue */
 #define MAGENTA "\033[35m" /* Magenta */
-#define CYAN "\
+#define CYAN "\033[36m" /* Cyan */
+#define WHITE "\033[37m" /* White */
+#define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
+#define BOLDRED "\033[1m\033[31m" /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m" /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m
