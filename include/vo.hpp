521c01c52fe8abd6a2b4be419ae1b1e9792a9a41
cosmonauto <cosmonautoyu@proton.me>
2023-06-07 09:10:13 +0000

    date format

--- include/vo.hpp
+++ include/vo.hpp
@@ -7,4 +7,13 @@ Descirption: This a sample code of my PhD works
 #ifndef VO_H
 #define VO_H
 
-#include <qu
+#include <quadmatcher.hpp>
+#include "opencv2/core/core.hpp"
+
+class VisualOdometry {
+
+public:
+
+  // camera parameters (all are mandatory / need to be supplied)
+  struct calibration {  
+    double f;  //
