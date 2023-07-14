7bbc87fd26ba04c5ab7049551478ce15cabaab2e
cosmonauto <cosmonautoyu@proton.me>
2023-07-14 06:35:11 +0000

    version 0.11.0

---
+++ src/mapper.cpp
@@ -0,0 +1,7 @@
+#include "mapper.h"
+
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+//#include <pcl/filters/
