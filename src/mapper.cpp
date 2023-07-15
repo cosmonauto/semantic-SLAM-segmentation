c30d352db3723b2beb519443292416dca4816b9b
cosmonauto <cosmonautoyu@proton.me>
2023-07-15 14:54:30 +0000

    Fixed permissions.

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -11,4 +11,11 @@ using namespace rgbd_tutor;
 
 Mapper::PointCloud::Ptr Mapper::generatePointCloud( const RGBDFrame::Ptr &frame )
 {
- 
+    semantic_motion_fuse(frame);
+
+    PointCloud::Ptr tmp( new PointCloud() );
+    if ( frame->pointcloud == nullptr )
+    {
+        // point cloud is null ptr
+        frame->pointcloud = boost::make_shared<PointCloud>();
+#pragma o
