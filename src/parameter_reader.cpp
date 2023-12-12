b552a62f51111d2dbb9e14d01de2b6ba2b7df377
cosmonauto <cosmonautoyu@proton.me>
2023-12-12 22:43:49 +0000

    favico

--- src/parameter_reader.cpp
+++ src/parameter_reader.cpp
@@ -1,4 +1,8 @@
 #include "rgbdframe.h"
 #include "parameter_reader.h"
 
-rgbd_tutor::CAMERA
+rgbd_tutor::CAMERA_INTRINSIC_PARAMETERS rgbd_tutor::ParameterReader::getCamera() const
+{
+    static rgbd_tutor::CAMERA_INTRINSIC_PARAMETERS camera;
+    camera.fx = this->getData<double>("camera.fx");
+    camera.fy = this->getData<dou
