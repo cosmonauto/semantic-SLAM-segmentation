b17cad2386ed4e3812bf1d3a7eb8afc8dac2a077
cosmonauto <cosmonautoyu@proton.me>
2023-12-14 07:55:45 +0000

    #44

--- src/parameter_reader.cpp
+++ src/parameter_reader.cpp
@@ -5,4 +5,8 @@ rgbd_tutor::CAMERA_INTRINSIC_PARAMETERS rgbd_tutor::ParameterReader::getCamera()
 {
     static rgbd_tutor::CAMERA_INTRINSIC_PARAMETERS camera;
     camera.fx = this->getData<double>("camera.fx");
-    camera.fy = this->getData<dou
+    camera.fy = this->getData<double>("camera.fy");
+    camera.cx = this->getData<double>("camera.cx");
+    camera.cy = this->getData<double>("camera.cy");
+    camera.d0 = this->getData<double>("camera.d0");
+    camera.d1 = this->getData<double>
