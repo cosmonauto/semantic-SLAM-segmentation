4f038ca9122f1ed1eeca10758e3ca2160929b068
cosmonauto <cosmonautoyu@proton.me>
2024-02-09 08:36:30 +0000

    It builds!

--- src/quadmatcher.cpp
+++ src/quadmatcher.cpp
@@ -177,3 +177,223 @@ void QuadFeatureMatch::drawMatchesQuad(int time)
      for(int i = 0; i < num; i++)
      {
           cv::Point pt;
+          pt.x = quadmatches[i].u1c;
+          pt.y = quadmatches[i].v1c;
+
+          cv::Point pt2;
+          pt2.x = quadmatches[i].u1p;
+          pt2.y = quadmatches[i].v1p;
+
+          cv::circle(img_show,pt,1,red,2,8,0);
+          cv::line(img_show,pt,pt2,green,1,8,0);
+     }
+
+     cv::imshow("image sparse flow",img_show);
+     cv::waitKey(time);
+ }
+
+ void QuadFeatureMatch::drawMatchesSimple(int time)
+ {
+     cv::Mat img_show;
+     cvtColor(img_lc, img_show, CV_GRAY2BGR);
+
+     cv::Scalar red(0,0,255);
+     cv::Scalar green(0,255,0);
+     cv::Scalar blue(255,0,0);
+
+     int num = point_lc.size();
+
+     for(int i = 0; i < num; i++)
+     {
+         cv::Point2f pt = point_lc[i];
+         cv::circle(img_show,pt,1,red,2,8,0);
+     }
+
+     cv::imshow("Detected Features",img_show);
+     cv::waitKey(time);
+ }
+
+
+
+
+
+ void QuadFeatureMatch::init(int detector_type, int descriptor_type)
+ {
+    switch(detector_type)
+     {
+        case DET_FAST:
+        {
+            detector = FeatureDetector::create("FAST");
+            detector->set("nonmaxSuppression", true);
+            detector->set("threshold",20);
+            break;
+        }
+
+        case DET_FAST_ADAPT:
+        {
+          detector = new DynamicAdaptedFeatureDetector (new FastAdjuster(30,true),800,1000,10);
+          break;
+        }
+        case DET_FAST_GRID:
+        {
+         //detector = new GridAdaptedFeatureDetector(new FastFeatureDetector(10,true),800,10,10);
+
+         detector = new GridAdaptedFeatureDetector(new DynamicAdaptedFeatureDetector (new FastAdjuster(20,true),12,15,10),
+                                                   800,10,10);
+          break;
+        }
+        case DET_STAR:
+        {
+            detector = FeatureDetector::create("STAR");
+            detector->set("lineThresholdProjected",10);
+            detector->set("lineThresholdBinarized",8);
+            detector->set("suppressNonmaxSize",5);
+            detector->set("responseThreshold",30);
+            detector->set("maxSize", 16);
+            break;
+        }
+       case DET_STAR_ADAPT:
+       {
+            detector = new DynamicAdaptedFeatureDetector (new StarAdjuster(40),800,1000,10);
+            break;
+       }
+
+        case DET_STAR_GRID:
+        {
+         detector = new GridAdaptedFeatureDetector (new StarFeatureDetector(16,10,10,8,5),800,4,4);
+            break;
+        }
+
+        case DET_ORB:
+        {
+            detector = FeatureDetector::create("ORB");
+            //printParams(detector);
+            detector->set("WTA_K",2);
+            detector->set("scoreType",ORB::HARRIS_SCORE);
+            detector->set("patchSize",31);
+            detector->set("edgeThreshold",31);
+            detector->set("scaleFactor", 1.2f);
+            detector->set("nLevels", 5);
+            detector->set("nFeatures",800);
+            break;
+        }
+        case DET_SURF:
+        {
+            detector = FeatureDetector::create("SURF");
+            //printParams(detector);
+            detector->set("hessianThreshold", 400);
+            detector->set("nOctaves",4);
+            detector->set("nOctaveLayers",2);
+            detector->set("upright",1);
+            break;
+        }
+        case DET_SIFT:
+        {
+            detector = FeatureDetector::create("SIFT");
+            //printParams(detector);
+            detector->set("nFeatures", 1000);
+            detector->set("nOctaveLayers",3);
+            detector->set("contrastThreshold",0.04);
+            detector->set("edgeThreshold",10);
+            detector->set("sigma",1.6);
+            break;
+        }
+
+        case DET_GFTT:
+        {
+             detector = FeatureDetector::create("GFTT");
+             //printParams(detector);
+             detector->set("qualityLevel",0.04);
+             detector->set("minDistance",8);
+            break;
+        }
+
+        case DET_GFTT_GRID:
+        {
+         detector = new GridAdaptedFeatureDetector (new GoodFeaturesToTrackDetector(20,0.01,5),800,8,8);
+            //printParams(detector);
+           // detector->set("qualityLevel",0.05);
+           // detector->set("minDistance",5);
+            break;
+        }
+     }
+
+    if(mode_track == false)
+     {
+         switch(descriptor_type)
+         {
+            case DES_SIFT:
+            {
+                descriptor = DescriptorExtractor::create("SIFT");
+                distance_threshold = 8000.0f;
+                descriptor_binary = false;
+                break;
+            }
+            case DES_SURF:
+            {
+                descriptor = DescriptorExtractor::create("SURF");
+                distance_threshold = 0.3f;
+                descriptor_binary = false;
+                break;
+            }
+            case DES_BRISK:
+            {
+                descriptor = DescriptorExtractor::create("BRISK");
+                distance_threshold = 120.0f;
+                descriptor_binary = true;
+                break;
+            }
+            case DES_FREAK:
+            {
+                descriptor = DescriptorExtractor::create("FREAK");
+                distance_threshold = 100.0f;
+                descriptor_binary = true;
+                break;
+            }
+            case DES_ORB:
+            {
+                descriptor = DescriptorExtractor::create("ORB");
+                distance_threshold = 80.0f;
+                descriptor_binary = true;
+                break;
+            }
+         }
+     }
+
+ }
+
+void QuadFeatureMatch::extractDescriptor()
+{
+
+    if(!keypoint_lc.size() || !keypoint_lp.size()
+            ||!keypoint_rc.size() || !keypoint_rp.size())
+    {
+        cout<<"Please Detect Feature Points At First!"<<endl;
+        return;
+    }
+
+    {
+            double time_descriptor = (double)cv::getTickCount();
+            descriptor->compute( img_lc, keypoint_lc, descriptor_lc );
+            descriptor->compute( img_rc, keypoint_rc, descriptor_rc );
+            descriptor->compute( img_lp, keypoint_lp, descriptor_lp );
+            descriptor->compute( img_rp, keypoint_rp, descriptor_rp );
+
+            time_descriptor = ((double)cv::getTickCount() - time_descriptor)/cv::getTickFrequency()*1000;
+            cout<<"The feature descriptor extraction in 4 images costs "<<time_descriptor<<" ms"<<endl;
+     }
+}
+
+
+
+void QuadFeatureMatch::detectFeature()
+{
+    if(mode_track == false)
+    {
+        double time_detector = (double)cv::getTickCount();
+        detector->detect(img_lc,keypoint_lc);
+        detector->detect(img_rc,keypoint_rc);
+        detector->detect(img_lp,keypoint_lp);
+        detector->detect(img_rp,keypoint_rp);
+
+        time_detector = ((double)cv::getTickCount() - time_detector)/cv::getTickFrequency()*1000;
