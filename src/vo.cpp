55d741d1d7944b81cccade5cfca3b0caa257e9d3
cosmonauto <cosmonautoyu@proton.me>
2024-03-01 06:31:59 +0000

    tg3: Don't check undefined error bits in RXBD

--- src/vo.cpp
+++ src/vo.cpp
@@ -9,4 +9,15 @@ Descirption: This a sample code of my PhD works
 
 using namespace std;
 
-VisualOdometry::VisualOdometry (parameters param) : 
+VisualOdometry::VisualOdometry (parameters param) : param(param) {
+  J         = 0;
+  p_observe = 0;
+  p_predict = 0;
+  Tr_delta = cv::Mat::eye(4,4,CV_64F);
+  srand(0);
+}
+
+VisualOdometry::~VisualOdometry () {
+}
+
+bool VisualOdometry::u
